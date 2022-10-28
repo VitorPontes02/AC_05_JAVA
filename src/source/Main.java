0347e84a70717901d46d0c227f243385e986f6c7
Vitor Hugo Barreto Pontes <vitor.barreto.pontes@gmail.com>
2022-10-28 16:44:51 -0300

    Terminando print dos testes

--- src/source/Main.java
+++ src/source/Main.java
@@ -11,6 +11,7 @@ public class Main {
 		
 		
 		System.out.println("---------------------");
+		System.out.println("Insert");
 		System.out.println(teste.toString());
 		System.out.println(teste.insert(5,"A"));
 		System.out.println(teste.toString());
@@ -20,40 +21,84 @@ public class Main {
 		System.out.println(teste.toString());
 		System.out.println(teste.insert(7,"D"));
 		System.out.println(teste.toString());
+		System.out.println("---------------------");
 		
-		
+		System.out.println("---------------------");
+		System.out.println("Min()");
 		System.out.println(teste.min());
 		System.out.println(teste.removeMin());
+		System.out.println(teste.toString());
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("size()");
 		System.out.println(teste.size());
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("removeMin()");
 		System.out.println(teste.removeMin());
 		System.out.println(teste.removeMin());
 		System.out.println(teste.removeMin());
 		System.out.println(teste.toString());
 		System.out.println("---------------------");
 		
+		System.out.println("---------------------");
+		System.out.println("isEmpty()");
 		System.out.println(teste2.isEmpty());
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("put()");
 		System.out.println(teste2.put(5,"A"));
 		System.out.println(teste2.put(7,"B"));
 		System.out.println(teste2.put(2,"C"));
 		System.out.println(teste2.put(8,"D"));
 		System.out.println(teste2.put(2,"E"));
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("get()");
 		System.out.println(teste2.get(7));
 		System.out.println(teste2.get(4));
 		System.out.println(teste2.get(2));
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("size");
 		System.out.println(teste2.size());
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("remove()");
 		System.out.println(teste2.remove(5));
 		System.out.println(teste2.remove(2));
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("get()");
 		System.out.println(teste2.get(2));
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("isEmpty()");
 		System.out.println(teste2.isEmpty());
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("entrySet()");
 		System.out.println(teste2.entrySet());
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("keySet()");
 		System.out.println(teste2.keySet());
+		System.out.println("---------------------");
+		
+		System.out.println("---------------------");
+		System.out.println("values()");
 		System.out.println(teste2.values());
 		
-		
-		
-		
-		
-
 
 
 	}
